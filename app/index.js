7482e71302886fd5fcd4cf978014209d3b25e260
Achilleas Tsoumitas <knorcedger@gmail.com>
2016-01-12 01:22:22 +0200

    Renamed the original file to gitignore (no dot)

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ var generators = require('yeoman-generator');
 module.exports = generators.Base.extend({
 	writing: function() {
 		this.fs.copyTpl(
-			this.templatePath('.gitignore'),
+			this.templatePath('gitignore'),
 			this.destinationPath('.gitignore')
 		);
 	}
